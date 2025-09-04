3f72e74bd72af538df839e9243d195072de54561
adityas7999 <shermaleaditya30@gmail.com>
2025-09-04 15:32:48 +0530

    Update app.py
    change the password , database names in config . also check  for the html in templates folder . update it with your  html code

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 db_config = {
     'host': 'localhost',
     'user': 'root',
-    'password': 'password',
+    'password': 'password ',
     'database': 'testdb',
     'cursorclass': pymysql.cursors.Cursor
 }
